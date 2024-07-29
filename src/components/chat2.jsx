abc5fa15c264627c7c7bc2cb3cd7786c7d5e3e18
MudasserAhmed <mudasserahmed808@gmail.com>
2024-07-29 11:28:23 +0500

    small changes

--- src/components/chat2.jsx
+++ src/components/chat2.jsx
@@ -63,14 +63,13 @@ const Chat2 = () => {
                     console.log(error);
                 }
             }
-
             const newMessage = {
                 text: messageText,
                 username: userName,
                 file: fileData,
                 timestamp: new Date().toISOString()
             };
-
+            console.log("message ",newMessage)
             sendMessage(newMessage);
             setMessageText('');
             setSelectedFile(null);
@@ -79,6 +78,7 @@ const Chat2 = () => {
     };
 
     const handleFileChange = (event) => {
+        console.log("selected file",event.target.files[0])
         const file = event.target.files[0];
         if (file && file.size <= 25 * 1024 * 1024) {
             console.log(file);
@@ -88,7 +88,6 @@ const Chat2 = () => {
             alert("File size exceeds 25 MB limit.");
         }
     };
-
     const handleKeyDown = (event) => {
         if (event.key === 'Enter') {
             event.preventDefault();
@@ -117,37 +116,41 @@ const Chat2 = () => {
     return (
         <div className={`flex flex-col h-screen w-[50%] mx-auto border border-gray-300 shadow-md rounded-lg overflow-hidden ${darkMode ? 'bg-gray-900 text-white' : 'bg-white text-gray-900'}`}>
             <header className="bg-[#004890] font-serif text-white p-4  text-xl font-semibold flex justify-between items-center">
-               <span> <img src="https://cdn.prod.website-files.com/6476dfb953dc2bc84373e1b2/650d2c2e05326b2de5695b99_JS%20Bank%20Logo-%20White.png" className='w-23 h-10' alt="" />  </span> 
-                <span>JS Chat</span>
+                <span> {userName}</span>
+                <span> <img src="https://cdn.prod.website-files.com/6476dfb953dc2bc84373e1b2/650d2c2e05326b2de5695b99_JS%20Bank%20Logo-%20White.png" className='w-23 h-10' alt="" />  </span>
+                {/* <span>JS Chat</span> */}
                 <button className='text-sm border border-slate-950 p-2 rounded-md bg-gray-400' onClick={handleLogout}>Reset Chat</button>
             </header>
             <div ref={messageContainerRef} className="flex-1 overflow-auto p-4 bg-gray-100">
                 <div className="space-y-4">
-                    {messages?.map((message, index) => (
-                        <div key={index} className={`flex ${message.username === userName ? 'justify-end' : 'justify-start'}`}>
-                            {message.username !== userName && (
-                                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpd4mJRIUwqgE8D_Z2znANEbtiz4GhI4M8NQ&s" alt="Logo" className="w-10 h-10 mr-3 rounded-full" />
-                            )}
-                            <div className={`p-3 rounded-lg max-w-xs ${message.username === userName ? 'bg-[#004890] text-white' : 'bg-gray-300 text-gray-800'}`}>
-                                <div className="flex items-center">
-                                    <div>
-                                        <p className='break-words'>{message.text}</p>
-                                        {message.file && (
-                                            <button onClick={() => handleFileDownload(message.file)} className="text-sm text-black underline">
-                                                Download {message.file.fileName}
-                                            </button>
-                                        )}
-                                        <p className="text-xs text-gray-500 mt-2">
-                                            {new Date(message.timestamp).toLocaleString()}
-                                        </p>
+                    {messages?.map((message, index) => {
+                        console.log(message)
+                        return (
+                            <div key={index} className={`flex ${message.username === userName ? 'justify-end' : 'justify-start'}`}>
+                                {message.username !== userName && (
+                                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpd4mJRIUwqgE8D_Z2znANEbtiz4GhI4M8NQ&s" alt="Logo" className="w-10 h-10 mr-3 rounded-full" />
+                                )}
+                                <div className={`p-3 rounded-lg max-w-xs ${message.username === userName ? 'bg-[#004890] text-white' : 'bg-gray-300 text-gray-800'}`}>
+                                    <div className="flex items-center">
+                                        <div>
+                                            <p className='break-words'>{message.text}</p>
+                                            {message.file && (
+                                                <button onClick={() => handleFileDownload(message.file)} className="text-sm text-black underline">
+                                                    Download {message.file.fileName}
+                                                </button>
+                                            )}
+                                            <p className="text-xs text-gray-500 mt-2">
+                                                {new Date(message.timestamp).toLocaleString()}
+                                            </p>
+                                        </div>
                                     </div>
                                 </div>
+                                {message.username === userName && (
+                                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpd4mJRIUwqgE8D_Z2znANEbtiz4GhI4M8NQ&s" alt="Logo" className="w-10 h-10 ml-3 rounded-full" />
+                                )}
                             </div>
-                            {message.username === userName && (
-                                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpd4mJRIUwqgE8D_Z2znANEbtiz4GhI4M8NQ&s" alt="Logo" className="w-10 h-10 ml-3 rounded-full" />
-                            )}
-                        </div>
-                    ))}
+                        )
+                    })}
                 </div>
             </div>
             <div className="bg-gray-100 p-4 border-t border-gray-200 flex items-center space-x-2">
